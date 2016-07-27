e0c016a6b5970149eb12d55c47d81c37c7315376
pfgithub <omegariisthebest@gmail.com>
2016-07-26 21:16:49 -0400

    Fix web interface

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var fileUpload = require('express-fileupload');
 
 var convert = require("./converter.js")
 
 app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({}));
+app.use(fileUpload());
 
 app.get('/', function (req, res) {
   res.render('controlpanel');
@@ -15,21 +17,28 @@ app.get('/', function (req, res) {
 function getObjectFor(body,letters){
   var ans = {};
   (letters.split ? letters.split(",") : letters).forEach(function(l){
-    ans[l] = body[l];
+    ans[l] = parseFloat(body[l]);
   });
   return ans;
 }
 
 app.post('/get.php', function(req,res){
   // s - scale, o - output, l - rotation-cutoff, d - depth, e - scratch, f- crease, ho - height-offset, p - crease-offset, m - x-multiplier, n - y-multiplier, c - crease-input
-  console.log(req);
+  //console.log();
+  var objr = getObjectFor(req.body,"s,l,d,e,f,ho,p,m,n");
+  objr.c = true;
+  console.log(objr);
   var result = convert(
-    getObjectFor(req.body,"l,d,e,f,ho,p,m,n"),
-    "input...",
-    "creaseinput..."
+    objr,
+    req.files.cut.data.toString("utf8"),
+    req.files.crease ? req.files.crease.data.toString("utf8") : undefined
   );
 
-  res.send(result);
+  console.log(req.body);
+
+  res.setHeader('Content-type', "application/octet-stream");
+  res.setHeader('Content-disposition', 'attachment; filename='+req.body.name+(new Date()).getTime()+'.tap');
+  res.send( new Buffer(result) );
 });
 
 app.listen(process.env.port || 3000, function () {
